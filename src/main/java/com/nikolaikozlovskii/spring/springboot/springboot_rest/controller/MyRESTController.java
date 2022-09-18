74e63596389d1f811a9af23d0c396b061d577637
Nikolai <nik-neman@mail.ru>
2022-09-18 11:21:19 +0200

    catch bags

--- src/main/java/com/nikolaikozlovskii/spring/springboot/springboot_rest/controller/MyRESTController.java
+++ src/main/java/com/nikolaikozlovskii/spring/springboot/springboot_rest/controller/MyRESTController.java
@@ -40,7 +40,7 @@ public class MyRESTController {
 
     @DeleteMapping("/employees/{id}")
     public String deleteEmployee(@PathVariable int id){
-        Employee employee = employeeService.getEmployee(id);
+        employeeService.deleteEmployee(id);
 
             return "Employee with ID = " + id + " was deleted";
 
